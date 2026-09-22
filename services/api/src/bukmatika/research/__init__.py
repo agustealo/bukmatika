efc0fea82d7ac74bfac2d9213fc8bedcf79d7c29
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 13:46:13 -0400

    Export grounded research contracts

--- services/api/src/bukmatika/research/__init__.py
+++ services/api/src/bukmatika/research/__init__.py
@@ -1,12 +1,29 @@
 from bukmatika.research.domain import (
+    GroundedAnswerDraft,
+    GroundedAnswerResponse,
+    GroundedAnswerStatus,
+    GroundedCitationResponse,
+    GroundedClaimDraft,
+    GroundedClaimResponse,
+    GroundedResearchRequest,
     ResearchPassageResponse,
     ResearchSearchRequest,
     ResearchSearchResponse,
 )
+from bukmatika.research.grounded import GroundedCitationInvalid, GroundedResearchService
 from bukmatika.research.routes import router
 from bukmatika.research.service import ResearchSelectionDenied, ResearchService
 
 __all__ = [
+    "GroundedAnswerDraft",
+    "GroundedAnswerResponse",
+    "GroundedAnswerStatus",
+    "GroundedCitationInvalid",
+    "GroundedCitationResponse",
+    "GroundedClaimDraft",
+    "GroundedClaimResponse",
+    "GroundedResearchRequest",
+    "GroundedResearchService",
     "ResearchPassageResponse",
     "ResearchSearchRequest",
     "ResearchSearchResponse",

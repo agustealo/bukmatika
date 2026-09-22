3004f8b8260797ae004b612e53963d3ae144ad85
Agustealo Johnson <agustealo@gmail.com>
2026-09-21 21:13:53 -0400

    Export processing source conflict

--- services/api/src/bukmatika/processing/__init__.py
+++ services/api/src/bukmatika/processing/__init__.py
@@ -11,6 +11,7 @@ from bukmatika.processing.service import (
     DocumentProcessingService,
     ProcessedDocumentNotFound,
     ProcessingAssetNotFound,
+    ProcessingSourceChanged,
     ProcessingTimedOut,
     StoredObjectUnavailable,
 )
@@ -25,6 +26,7 @@ __all__ = [
     "ParserRegistry",
     "ProcessedDocumentNotFound",
     "ProcessingAssetNotFound",
+    "ProcessingSourceChanged",
     "ProcessingTimedOut",
     "StoredObjectUnavailable",
     "TextDocumentParser",
